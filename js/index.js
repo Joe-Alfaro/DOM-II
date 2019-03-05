f9001972e6bb2ab6b632dfd00a8a6e912758c32e
joe-alfaro <josephlalfaro@gmail.com>
2019-03-05 12:44:11 -0800

    added moseover functionality to a tags in navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+let links = document.getElementsByClassName("nav-link");
+for(let i = 0; i < links.length; i++){
+    links[i].addEventListener("mouseover", function(){
+      links[i].style.color = "red";
+    })
+
+    links[i].addEventListener("mouseout", function(){
+      links[i].style.color = "black";
+    })
+}
